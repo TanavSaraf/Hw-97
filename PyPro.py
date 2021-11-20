0605ed4117766200088dbfd66323a1687eb3e00a
TanavSaraf <75167346+TanavSaraf@users.noreply.github.com>
2021-11-20 16:26:11 +0530

    Add files via upload

--- PyPro.py
+++ PyPro.py
@@ -1,24 +1,49 @@
-import random 
-randomNo=random.randint(0,10)
-inputss=int(input("Enter a guess between 1-9:"))
-chances=5
-if inputss<10 : 
-    
-   
-    if(inputss!=randomNo and chances==5):
-        print("4 chances more to change your destiny")
-        
-    elif (inputss!=randomNo and chances==4):
-        print("3 chances more to change your destiny")
-    elif (inputss!=randomNo and chances==3):
-        print("2 chances more to change your destiny")
-    elif (inputss!=randomNo and chances==2):
-        print("1 chances more to change your destiny")
-    elif (inputss!=randomNo and chances==1):
-          print("YOU SHOULD HAVE BEEN CAREFUL ")    
+import random
+randomNo = random.randint(0, 10)
+inputss = int(input("Enter a guess between 1-9:"))
+chances = 5
+if inputss < 10:
+    if(inputss==randomNo):
+        print("you won")
     else :
-         print("you won")
-        
-        
-else :
+        print("4 chances more to change your destiny")
+        inputss = int(input("Enter a guess between 1-9:"))
+        chances=4
+        if(inputss==randomNo):
+            print("you won")
+        else :    
+                print("3 chances more to change your destiny")
+                inputss = int(input("Enter a guess between 1-9:"))
+                chances=3
+                if(inputss==randomNo):
+                    print("you won")
+                else :    
+                            print("2 chances more to change your destiny")
+                            inputss = int(input("Enter a guess between 1-9:"))
+                            chances=2
+                            if(inputss==randomNo):
+                                print("you won")
+                            else :    
+                                    print("1 chances more to change your destiny")
+                                    inputss = int(input("Enter a guess between 1-9:"))
+                                    chances=1
+                                    if(inputss==randomNo):
+                                        print("you won")  
+                                    else :
+                                        print("you successfully lost this game :-)")
+                                        print()
+                                        print("the answer was :",randomNo)
+         
+   
+else:
     print("Can you not follow instructions!")
+# elif (inputss!=randomNo and chances==4):
+  #   print("3 chances more to change your destiny")
+ #elif (inputss!=randomNo and chances==3):
+   #  print("2 chances more to change your destiny")
+ #elif (inputss!=randomNo and chances==2):
+    # print("1 chances more to change your destiny")
+ #elif (inputss!=randomNo and chances==1):
+     #  print("YOU SHOULD HAVE BEEN CAREFUL ")    
+ #else :
+      #print("you won")
